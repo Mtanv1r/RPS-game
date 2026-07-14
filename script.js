5e87d44f7e7c369d7a40d2e65b69930230f31861
tanvir <oxyzen2416256@gmail.com>
2026-07-14 20:14:42 +0600

    random num gen created

---
+++ script.js
@@ -0,0 +1,7 @@
+const arr=["rock","paper","scissors"];
+//console.log(arr[0]);
+//console.log(arr.length);
+//random number generator
+const randomIndex=Math.floor(Math.random()*arr.length);
+//console.log(randomIndex);
+console.log(arr[randomIndex]);
